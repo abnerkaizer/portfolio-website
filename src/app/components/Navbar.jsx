627c5cf22e14f9ec8c53f3819a72f1dbbaae0ee9
Abner Kaizer <abnerkaizer@protonmail.com>
2024-04-06 10:34:15 -0300

    Logo

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
+import Image from 'next/image'
 
 const navLinks = [
   {
@@ -30,7 +31,9 @@ const Navbar = () => {
           href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold"
         >
-          LOGO
+          <span className="text-muted">
+            <Image src={"/images/ASK-no-bg.png"} width={90} height={90} alt='Logo'></Image>
+          </span>
         </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
