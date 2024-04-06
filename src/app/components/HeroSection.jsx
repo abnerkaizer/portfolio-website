5b18d8c1bf02b1d5a1c0da1e038a97b83dc7b572
Abner Kaizer <abnerkaizer@protonmail.com>
2024-04-06 11:26:15 -0300

    Resume link.

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
-
+import Link from "next/link";
 const HeroSection = () => {
   return (
     <section className="lg:py-16">
@@ -35,11 +35,11 @@ const HeroSection = () => {
           </p>
           <div>
             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white">
-              Hire Me
+              Hire me           
             </button>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
+              <Link href={"https://drive.google.com/file/d/1EMQZYSQsX1UeC7Alco5ZJg7yeUNWvjzo/view?usp=sharing"}>Download CV</Link> 
               </span>
             </button>
           </div>
