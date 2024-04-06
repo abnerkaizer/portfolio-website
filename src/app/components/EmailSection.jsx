ce65a4d512c51c281317bb3d6754edc7bae61702
Abner Kaizer <abnerkaizer@protonmail.com>
2024-04-06 12:35:31 -0300

    GitHub and LinkedIn profiles.

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -57,10 +57,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="github.com/abnerkaizer">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="linkedin.com/in/abnerkaizer">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
