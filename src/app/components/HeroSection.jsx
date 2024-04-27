18934b5810d4a5b443d4e1f0063ee7848b7d0b25
Abner Kaizer <contact@abnerkaizer.com>
2024-04-27 20:17:54 -0300

    Mudou link.

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -39,7 +39,7 @@ const HeroSection = () => {
             </button>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-              <Link href={"https://pcloud.live/index.php/s/dzqi3AdRto9KA3j"}>Download CV</Link> 
+              <Link href={"https://pcloud.live/index.php/s/R4iHGP6GiM7GE63"}>Download CV</Link> 
               </span>
             </button>
           </div>
