517641569e65377f98eccf58ec8fd660394a7b09
Abner Kaizer <contact@abnerkaizer.com>
2024-04-27 14:55:57 -0300

    Moved resume to nextcloud

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -39,7 +39,7 @@ const HeroSection = () => {
             </button>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-              <Link href={"https://drive.google.com/file/d/1EMQZYSQsX1UeC7Alco5ZJg7yeUNWvjzo/view?usp=sharing"}>Download CV</Link> 
+              <Link href={"https://atrocious.cloud/index.php/s/WQG4AKbEe8cMwJe"}>Download CV</Link> 
               </span>
             </button>
           </div>
